df97d453a02376e4c9fa3bf2ee930f3bf351a367
baris.ozen <bilgiislem@balikesirgonen.bel.tr>
2023-03-09 17:18:48 +0300

    ..

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name='index')
+    path('', views.index, name='index'),
+    path('books', views.books, name='bookslar'),
+    path('authors', views.authors, name='authors'),
+    path("authordetails/<int:authorId>", views.authordetails, name="authordetails")
 ]
--- books/views.py
+++ books/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Anasayfa Merhaba Dünya")
+def authors(request):
+    return HttpResponse("Yazarların Listesi Geldi Buraya heralde kodlar gelecek")
+def books(request):
+    return HttpResponse("Kitapların Listesi Geldi Buraya heralde kodlar gelecek")
+def authordetails(request,authorId):
+    return HttpResponse("Yazarların Detay Listesi Geldi Buraya heralde kodlar gelecek" + authorId)
