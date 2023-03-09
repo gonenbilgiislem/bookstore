9996ef796b7a5212f8bf6e532099d3adcbc10da6
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-03-10 01:20:20 +0300

    .

--- books/views.py
+++ books/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Anasayfa Merhaba Dünya")
+    return render(
+        request, 'index.html')
 def authors(request):
     return HttpResponse("Yazarların Listesi Geldi Buraya heralde kodlar gelecek")
 def books(request):
     return HttpResponse("Kitapların Listesi Geldi Buraya heralde kodlar gelecek")
 def authordetails(request,authorId):
-    return HttpResponse("Yazarların Detay Listesi Geldi Buraya heralde kodlar gelecek" + authorId)
+    return HttpResponse("Yazarların Detay Listesi Geldi Buraya heralde kodlar gelecek : " + str(authorId))
